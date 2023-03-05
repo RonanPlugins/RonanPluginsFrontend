7b944b878857ba292609b93e1125cb5897e445d6
ARR4NN <arranhardy07@gmail.com>
2023-03-05 19:30:41 +0000

    fix

---
+++ src/pages/Account/Connections.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Connections = () => {
+  return (
+      <div>
+          
+          <button>Connect to discord</button>
+    </div>
+  )
+}
+
+export default Connections
