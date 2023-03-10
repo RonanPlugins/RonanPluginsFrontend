edd1beeabccb48285e0a57655c120e2a8242238a
ARR4NN <arranhardy07@gmail.com>
2023-03-10 22:24:15 +0000

    changes + mockdata

--- src/pages/main/Plugins.jsx
+++ src/pages/main/Plugins.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+const PluginsData = require('../../mockupData/Plugins.json');
+import './Plugins.css';
 
 const Plugins = () => {
-  return (
-    <div>Plugins Page</div>
-  )
-}
+ return (
+  <div className="container">
+   <div key="1" className='top' >1</div>
+   <div key="2" className='middle'>2</div>
+   <div key="3"className='bottom'>3</div>
+  </div>
+ );
+};
 
-export default Plugins
+export default Plugins;
