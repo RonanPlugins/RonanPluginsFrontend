6cd36af28d83b22332835205812087911b6fe0a2
ARR4NN <arranhardy07@gmail.com>
2023-03-17 21:38:48 +0000

    yea

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const client = axios.create({
-    baseURL: "http://localhost:3001",
+    baseURL: "https://api.ronanplugins.com",
     responseType: "json",
     withCredentials: true,
 });
