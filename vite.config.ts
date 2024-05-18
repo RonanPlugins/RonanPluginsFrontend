5eb8e849bc4fd2ea90c1e75ef0de40a7de3b1807
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-18 17:59:33 -0400

    shadcn install

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // eslint-disable-next-line no-undef
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: 3000,
     open: true,
