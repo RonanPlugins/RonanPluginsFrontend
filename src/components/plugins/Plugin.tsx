8fdef6348e84937b75da73560c9c596b561113a8
RonanCraft <SuperRonanCraft@gmail.com>
2024-05-19 02:29:32 -0400

    plugins and home page

--- src/components/plugins/Plugin.tsx
+++ src/components/plugins/Plugin.tsx
@@ -1,27 +1,62 @@
 // const PluginsData = require("../../../mockupData/Plugins.json");
 // import { Rating } from "react-simple-star-rating";
 
+import { Star } from "lucide-react";
+import { Card, CardContent, CardFooter, CardTitle } from "../ui/card";
+import { Ratings } from "../ui/rating";
+import { useLocation } from "react-router-dom";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+
 export default function Plugin({ plugin }) {
+  console.log(plugin);
   return (
-    <a href={"https://www.spigotmc.org/resources/" + plugin.id}>
-      <div className="plugin">
-        <img src={"https://www.spigotmc.org/" + plugin.icon.url} />
-        <div className="Content">
-          <div className="Name">{plugin.name}</div>
-          <div className="Stars">
-            {plugin.rating.average}
-            {/* <Rating
-                      initialValue={plugin.rating.average}
-                      readonly={true}
-                      allowFraction={true}
-                      size={20}
-                      SVGstyle={{ display: "inline-block" }}
-                    /> */}
+    // <a href={"https://www.spigotmc.org/resources/" + plugin.id}>
+    <Card className="text-center">
+      <CardTitle className="truncate mx-2 my-2">
+        {plugin.name}
+        <p className="text-sm font-normal text-primary">
+          {new Date(plugin.updateDate * 1000).toLocaleString()}
+        </p>
+      </CardTitle>
+      <CardContent>
+        <img
+          className="mx-auto my-2 h-20"
+          src={"https://www.spigotmc.org/" + plugin.icon.url}
+        />
+        <div className="flex">
+          <div className="grow"></div>
+          <div className="grow-0">
+            <Ratings rating={plugin.rating.average} />
           </div>
-          <div className="Reviews">({plugin.rating.count})</div>
-          <div className="Mini-Description">{plugin.tag}</div>
-        </div>
-      </div>
-    </a>
+          <div className="grow"></div>
+        </div>{" "}
+        <p className="text-sm text-primary/80">{plugin.rating.count} ratings</p>
+        <p className="text-primary">{plugin.downloads} downloads</p>
+      </CardContent>
+      <CardFooter className="pr-2 pb-2 flex justify-between">
+        {plugin.premium && (
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Star className="fill-current text-yellow-500"></Star>
+              </TooltipTrigger>
+              <TooltipContent>Premium</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        )}
+        <a
+          href={"https://www.spigotmc.org/resources/" + plugin.id}
+          target="_blank"
+          className="text-right ml-auto text-muted text-sm hover:underline"
+        >
+          View on Spigot...
+        </a>
+      </CardFooter>
+    </Card>
   );
 }
--- src/pages/Plugins.tsx
+++ src/pages/Plugins.tsx
@@ -11,7 +11,7 @@ const Plugins = () => {
     const fetchData = async () => {
       axios
         .get(
-          "https://api.spiget.org/v2/authors/13025/resources?size=1000&sort=-downloads"
+          "https://api.spiget.org/v2/authors/13025/resources?size=32&sort=-downloads"
         )
         .then((response) => {
           if (response.status === 200) {
@@ -30,9 +30,9 @@ const Plugins = () => {
   if (loading) return <>Loading...</>;
 
   return (
-    <div className="plugins">
+    <div className="my-10 grid grid-cols-3 gap-3 mx-10">
       {plugins.map((plugin) => (
-        <Plugin plugin={plugin} />
+        <Plugin key={plugin.id} plugin={plugin} />
       ))}
     </div>
   );
