0707e6041533c14c6fdf64028752126640425cea
RonanCraft <SuperRonanCraft@gmail.com>
2024-06-20 10:07:34 -0400

    postinstall linux fix

--- postinstall.js
+++ postinstall.js
@@ -1,9 +1,20 @@
 // Used to install TinyMCE (Text Editor) so we dont need an API key (this self hosts the packages)
-import fse from "fs-extra";
+import fs from "fs-extra";
 import path from "path";
-const topDir = import.meta.dirname;
-fse.emptyDirSync(path.join(topDir, "public", "tinymce"));
-fse.copySync(
+import { fileURLToPath } from "url";
+
+// Get the directory name of the current module file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the top directory
+const topDir = __dirname;
+
+// Empty the target directory
+fs.emptyDirSync(path.join(topDir, "public", "tinymce"));
+
+// Copy the contents from source to target
+fs.copySync(
   path.join(topDir, "node_modules", "tinymce"),
   path.join(topDir, "public", "tinymce"),
   { overwrite: true }
